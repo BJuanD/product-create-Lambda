8aa2a70734704544f3b3ed159f2dc07e5580462c
BJuanD <berteljuan77@gmail.com>
2025-05-23 10:50:43 -0500

    api faltante

---
+++ lambdas.tf
@@ -0,0 +1,15 @@
+resource "aws_lambda_function" "create_product" {
+  function_name = "create-product"
+  filename      = "lambda/product-create/create.zip"
+  handler       = "index.handler"
+  runtime       = "nodejs18.x"
+  source_code_hash = filebase64sha256("lambda/product-create/create.zip")
+
+  role = aws_iam_role.lambda_role.arn
+
+  environment {
+    variables = {
+      #JWT_SECRET = "clave_super_segura"
+    }
+  }
+}
